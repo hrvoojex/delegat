7e3e4f8522ef83fd3cda177466ffa0d31b0348fa
Hrvoje T <hrvooje@gmail.com>
2020-12-01 22:39:12 +0100

    Initial commit with test form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms'
+import competitionJson from "../../assets/competitions.json";
+
+interface ICompetition {
+    "name": string
+  }
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  competitions: ICompetition[] = competitionJson.competitions;
+
+  firstNameDelegate: string = '';
+  lastNameDelegate: string = '';
+  selectedCompetition: string = '';
+  addition: string = '';
+
+  competitionForm: FormGroup;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit() {
+    this.competitionForm = this.fb.group({
+      competitionControl: [''],
+      additionControl: ['']
+    });
+
+    console.log(this.competitions)
+
+  }
+
+}
